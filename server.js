fb6bdcdfc15591020268549062065adee1b144c4
Maulian_Santos <mauliansantoscruz@gmail.com>
2024-11-09 16:52:46 -0300

    Dividindo api em camadas

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});

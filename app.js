3f71bf8487bdf4e2a7eb651cff9602a7727484b2
Matheus Henrique <mathsmeireles@gmail.com>
2024-11-29 17:40:50 -0300

    add contagem de tempo de inicialização

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const livroRoutes = require('./routes/livroRoutes');
 
+console.time('Tempo de inicialização');
+
 app.use(express.json());
 app.use('/api', livroRoutes);
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  console.timeEnd('Tempo de inicialização');
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
